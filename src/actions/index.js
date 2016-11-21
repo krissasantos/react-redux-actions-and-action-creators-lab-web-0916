c023734240da2f32483aaee0a0c7c3bd134a57ea
krissasantos <krissa.santos31@gmail.com>
2016-11-21 14:07:29 -0500

    Done.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,39 @@
 // define your actions here!
+
+// export function incrementCount(){
+//   return {
+//     type: 'INCREMENT_COUNT'
+//   }
+// }
+
+  
+
+export function addMovie(movie){
+    return {
+     type: 'ADD_MOVIE',
+     movie: movie
+    };
+}
+
+// let addMovie = addMovie();
+// store.dispatch( addMovie ) ;
+
+export function deleteMovie(movie){
+    return {
+     type: 'DELETE_MOVIE',
+     movie: movie
+    };
+}
+
+export function updateUsername(name){
+    return {
+     type: 'UPDATE_USERNAME',
+     username: name
+    };
+}
+export function resetUsername(){
+    return {
+     type: 'RESET_USERNAME',
+     username: 'default'
+    };
+}
